0e42f40e7fbe09f55d07914900b2c8d09bf2da9f
Yehiyam Livneh <yehiyam@gmail.com>
2020-12-23 17:26:22 +0200

    fix tests

--- tests/test.js
+++ tests/test.js
@@ -7,13 +7,17 @@ const cleaner = require('../lib/cleaner/cleaner');
 const storeManager = require('../lib/store/store-manager');
 const apiServer = require('../lib/api-server-client');
 const executions = require('./mocks/executions');
-
+const configIt = require('@hkube/config');
+const { main, logger } = configIt.load();
 describe('dummy test', () => {
     before(async () => {
         nock('http://localhost:3000').persist().post('/internal/v1/exec/stop').reply(200);
         await bootstrap.init();
-        await storeManager._db.jobs.createMany(executions.map((e, i) => ({ jobId: `job-${i}`, pipeline: e })));
     });
+    beforeEach(async () => {
+        await storeManager.init(main)
+        await storeManager._db.jobs.createMany(executions.map((e, i) => ({ jobId: `job-${i}`, pipeline: e })));
+    })
     it('should clean only expired pipelines', async () => {
         const spyStop = sinon.spy(apiServer, "stop");
         await cleaner.clean();
